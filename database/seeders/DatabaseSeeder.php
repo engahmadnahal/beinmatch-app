ed302b7bceaa54626ffa46e1c2cb36c6e87a83ba
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-06-12 20:47:29 +0300

    Create seeder for admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,5 +40,21 @@ class DatabaseSeeder extends Seeder
             'status' => "active",
         ]);
 
+        Employee::create([
+            'fname'=>"Admin",
+            'lname'=>"Admin",
+            'username'=>"Admin",
+            'avater'=>"assets/img/upload/media/login.png",
+            'email'=>"admin@admin.com",
+            'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'salary'=>"000000",
+            'jop_title'=>'Admin',
+            'type'=>"admin",
+            'phone'=>"0123456789",
+            'gender'=>"M",
+            'is_online'=>"true",
+            'status'=>"active"
+        ]);
+
     }
 }
