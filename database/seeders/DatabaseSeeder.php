c761e38ecdf92b2374002d6027b41bf94331d925
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-06-13 09:59:57 +0300

    FInsh Seeder For Create Setting mobile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\DawryFollower ;
 use App\Models\Employee;
 use App\Models\Like;
 use App\Models\Post;
+use App\Models\Setting;
 use App\Models\User;
 use App\Models\View;
 use Illuminate\Database\Seeder;
@@ -30,6 +31,10 @@ class DatabaseSeeder extends Seeder
         // Comment::factory(40)->create();
         // View::factory(25)->create();
         // Like::factory(20)->create();
+
+        /*
+            Create A Guset User
+        */
         User::create([
             'fname' => "Guest",
             'lname' => "User",
@@ -40,8 +45,9 @@ class DatabaseSeeder extends Seeder
             'status' => "active",
         ]);
 
-        //id	fname	lname	username	avater	email	email_verified_at	password	salary
-        // jop_title	phone	gender	address	is_online	status	deleted_at	created_at	updated_at
+        /**
+         * Create A Admin User
+         */
         Employee::create([
             'fname'=>"Admin",
             'lname'=>"Admin",
@@ -51,6 +57,15 @@ class DatabaseSeeder extends Seeder
             'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'salary'=>"000000",
             'jop_title'=>'Admin',
+        ]);
+
+        /**
+         * Create a setting for mobile
+         */
+        Setting::create([
+            "ads_active"=>0,
+            "match_active "=>1,
+            "slide_active"=>0,
         ]);
 
     }
