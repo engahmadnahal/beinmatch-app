7c22333f46439c8025e5d2bff345c938d7f331b8
Ahmad Nahal <ahmadGhssanNahal@gmail.com>
2022-06-19 15:28:40 +0300

    Add Veririy For Employee

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -62,6 +62,7 @@ class DatabaseSeeder extends Seeder
                 'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'salary'=>"000000",
                 'jop_title'=>'Admin',
+                'email_verified_at' => now(),
             ]);
          }
 
