f8b5609422fc4baf346b7d6e3939524982e190a7
engahmadnahal <ahmadghssannahal@gmail.com>
2022-07-30 00:47:39 +0300

    Add pagnite post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        $posts = Post::where('status', 'done')->orderBy('created_at','desc')->paginate(10);
+        $posts = Post::where('status', 'done')->orderBy('created_at','desc')->paginate(20);
         return new MainResource(PostResource::collection($posts),Response::HTTP_OK,'تم جلب الأخبار بنجاح');
     }
 
