f8af5764ad7e40406b406cac2d8407ad060570a3
engahmadnahal <ahmadghssannahal@gmail.com>
2022-08-08 00:25:09 +0300

    Error Pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -12,7 +12,6 @@
 			<img src="{{asset('assets/img/media/500.png')}}" class="error-page" alt="error">
 			<h2>Oopps. The page you were looking for doesn't exist.</h2>
 			<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger" href="{{route('home.index')}}">Back to Home</a>
-			<h6>Error | {{$exception->getMessage()}}</h6>
 		
 		</div>
         <a href="#top" id="back-to-top" style="display: none;"><i class="las la-angle-double-up"></i></a>
