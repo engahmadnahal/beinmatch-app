72f24b13b7946f1894961f627c72d928f404ed57
engahmadnahal <ahmadghssannahal@gmail.com>
2022-08-08 00:19:37 +0300

    Error Pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -9,9 +9,10 @@
 		</div>
 
        <div class="main-error-wrapper  page page-h ">
-			<img src="http://localhost/assets/img/media/404.png" class="error-page" alt="error">
+			<img src="{{asset('assets/img/media/404.png')}}" class="error-page" alt="error">
 			<h2>Oopps. The page you were looking for doesn't exist.</h2>
-			<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger" href="http://localhost/index">Back to Home</a>
+			<h6>You may have mistyped the address or the page may have moved.</h6><a class="btn btn-outline-danger" href="{{route('home.index')}}">Back to Home</a>
+			<h6>Error | {{$exception->getMessage()}}</h6>
 		</div>
         <a href="#top" id="back-to-top" style="display: none;"><i class="las la-angle-double-up"></i></a>
 @endsection
